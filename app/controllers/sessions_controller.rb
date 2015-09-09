1176f2786f68387bd325280512493781ffd97bcc
Tyler Graham <tylergraham134@gmail.com>
2015-09-09 14:40:58 -0600

    implements flash messages downloads fonts, fixtures, and glyphs minor styling changes - theme changed to Yeti

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     if @user
       session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.nickname}"
       redirect_to dashboard_path
-      # flash[:message] = "Welcome, #{@user.nickname}"
     else
       redirect_to root_path
     end
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    # flash[:message] = "You have logged out"
+    flash[:success] = "You have logged out"
     redirect_to root_path
   end
 end
