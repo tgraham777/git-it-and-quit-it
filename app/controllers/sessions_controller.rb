27ca6c3951e15fc6faab998668c0ee9883fe2354
Tyler Graham <tylergraham134@gmail.com>
2015-09-09 00:08:17 -0600

    working on heroku minor styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     if @user
       session[:user_id] = @user.id
       redirect_to dashboard_path
+      # flash[:message] = "Welcome, #{@user.nickname}"
     else
       redirect_to root_path
     end
@@ -11,6 +12,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    # flash[:message] = "You have logged out"
     redirect_to root_path
   end
 end
