7b295e8eb6e36593f5d89b21f4f271aa41ccf6f2
Tyler Graham <tylergraham134@gmail.com>
2015-09-08 19:56:43 -0600

    adds Figaro

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'omniauth-github'
 
+gem 'figaro'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, "ee2dde4ccf071151ba27", "64fc8ea43f67c3760ada1c99d6f6250683ef7110"
+  provider :github, ENV['github_id'], ENV['github_secret']
 end
