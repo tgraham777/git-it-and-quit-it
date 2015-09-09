c6c46a1cfad3c6ffd40ad33eeaf60995afd70e70
Tyler Graham <tylergraham134@gmail.com>
2015-09-08 21:48:15 -0600

    finishes Oauth implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/create'
 
-  # get 'welcome/index'
-
   get '/auth/github/callback', to: 'sessions#create'
-  # get '/logout', to: 'sessions#destroy'
-  # get '/dashboard', to: 'dashboard#show'
+  get '/logout', to: 'sessions#destroy'
+
+  get '/dashboard', to: 'dashboard#show'
 
   root 'welcome#show'
 end
