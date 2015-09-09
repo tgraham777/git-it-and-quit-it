ff27f73d71c6bffcbd85e4d1fab1f496e66c43a5
Tyler Graham <tylergraham134@gmail.com>
2015-09-08 22:35:43 -0600

    changes omniauth key titles to all capital letters

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['github_id'], ENV['github_secret']
+  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
 end
