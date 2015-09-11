10350e64c9601fe5593f2a764beb92da1ee7df52
Tyler Graham <tylergraham134@gmail.com>
2015-09-11 01:22:13 -0600

    minor styling changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     if @user
       session[:user_id] = @user.id
-      # flash[:message] = "Welcome, #{@user.nickname}"
       redirect_to dashboard_path
     else
       redirect_to root_path
@@ -12,7 +11,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:message] = "You have logged out"
     redirect_to root_path
   end
 end
